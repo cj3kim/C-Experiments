956416cc3c476abf64799a8562acdd1c04fc3307
Chris Kim <cj3kim@gmail.com>
2015-07-10 13:52:12 -0700

    Exploring how structs operate at the C and assembly level.

---
+++ struct_experiments/test_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+  int y;
+  int weight;
+  struct edgenode *next;
+} edgenode ;
+
+int main () {
+
+  edgenode *p;
+  p = malloc(sizeof(edgenode));
+}
+
